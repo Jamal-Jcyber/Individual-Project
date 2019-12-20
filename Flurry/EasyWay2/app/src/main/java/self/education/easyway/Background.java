352572988465e47aa060f0b6643cc76699f0e7a4
Jamal-Jcyber <AmimiJamal@outlook.fr>
2019-12-20 16:58:38 +0100

    Adding the new Project

---
+++ Flurry/EasyWay2/app/src/main/java/self/education/easyway/Background.java
@@ -0,0 +1,13 @@
+package self.education.easyway;
+
+import android.os.AsyncTask;
+
+public class Background extends AsyncTask<String, Void, String> {
+
+
+    @Override
+    protected String doInBackground(String... voids) {
+
+        return null;
+    }
+}
